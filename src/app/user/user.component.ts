1c743f051164889d4a0186319f9f83483bfab192
abdessamadjibrane <abdessamadjibrane2@gmail.com>
2025-02-08 22:57:01 +0100

    porject is finished but is still to be improved with adding services

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,17 +1,15 @@
 import { Component, EventEmitter, Input, Output} from '@angular/core';
-interface User{
-  id: string;
-  name: string;
-  avatar: string;
-}
+import {User} from'./user.model';
+import { CardComponent } from "../shared/card/card.component";
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({required: true}) selected! : boolean;
   @Input({required: true}) user! : User;
   @Output() select = new EventEmitter<string>();
 
